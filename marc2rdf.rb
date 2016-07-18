e24c66ff05092935bbaea0ca73801d37ec8dca02
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-07-18 13:45:24 +0200

    don't print timing, caller can choose to time it

--- marc2rdf.rb
+++ marc2rdf.rb
@@ -26,13 +26,8 @@ end; }
 mapping_file = $mapping_file ? $mapping_file : 'config/mapping.yaml'
 mapping = YAML.load(IO.read(mapping_file))
 
-start = Time.now
-
 marc2rdf = Marc2RDF.new(mapping, $host)
 marc_reader = MARC::XMLReader.new($input_file)
 for record in marc_reader
     STDOUT.puts marc2rdf.convert(record).dump(:ntriples)
 end
-
-diff = Time.now - start
-STDERR.puts("MARC2RDF conversion done in #{diff} seconds")
